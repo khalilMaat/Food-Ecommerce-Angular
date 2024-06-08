98e1c729a05c7299bd0a4d0366022c35588ae313
khalilMaat <maatoug620@outlook.fr>
2024-06-08 08:31:19 +0100

    front end

---
+++ src/app/module/client/client.module.ts
@@ -0,0 +1,36 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+import {ClientRoutingModule} from './client-routing.module';
+import {NavbarComponent} from './navbar/navbar.component';
+import {LoginComponent} from './auth/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {RegisterComponent} from './auth/register/register.component';
+import {SharedModule} from "../../shared/shared.module";
+import {RouterModule} from "@angular/router";
+import {CarouselModule} from "ngx-owl-carousel-o";
+import {NgbAccordionButton, NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import { IndexComponent } from './index/index.component';
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    IndexComponent
+
+  ],
+  imports: [
+    CommonModule,
+    ClientRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SharedModule,
+    RouterModule,
+    CarouselModule,
+    NgbModule
+  ]
+})
+export class ClientModule {
+}
